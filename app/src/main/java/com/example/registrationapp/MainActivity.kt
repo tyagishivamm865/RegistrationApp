63027a63fd97b489547fd6523fd6fde6dd6b9d1b
Shivam <tyagishivamm865@gmail.com>
2022-04-06 14:16:50 +0530

    Registration Ui page 2

--- app/src/main/java/com/example/registrationapp/MainActivity.kt
+++ app/src/main/java/com/example/registrationapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.registrationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btn:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn=findViewById(R.id.btnNext)
+
+        btn.setOnClickListener{
+            val intent = Intent(this,YourInfoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
