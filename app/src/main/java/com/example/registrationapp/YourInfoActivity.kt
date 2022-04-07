a72a6f5598dc7fb1488d59d2d6c2bc4ed3ba558f
Shivam <tyagishivamm865@gmail.com>
2022-04-07 13:24:25 +0530

    Registration Ui page 3

--- app/src/main/java/com/example/registrationapp/YourInfoActivity.kt
+++ app/src/main/java/com/example/registrationapp/YourInfoActivity.kt
@@ -1,11 +1,20 @@
 package com.example.registrationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class YourInfoActivity : AppCompatActivity() {
+    lateinit var btn:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_your_info)
+        btn=findViewById(R.id.Button03)
+
+        btn.setOnClickListener{
+            val intent = Intent(this,YourAdressActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
